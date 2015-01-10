bca30414492e658a8aabd7a1476756992b165a96
Zakhar Voit <zakharvoit@gmail.com>
2015-01-10 18:08:51 +0300

    Improve test scripts and add big test for the third task

--- test/test.pl
+++ test/test.pl
@@ -11,6 +11,10 @@ print colored ['yellow'], "Testing $task\n";
 while (glob "./test-files/$task/*.in") {
 	my $out = $_;
 	$out =~ s/in/out/;
+	if (/disable/) {
+	    print colored ['yellow'], "Ignored $_\n";
+	    next;
+	}
 	if (system("./$task.native <$_ >buffer.txt") != 0) {
 		print colored ['red'], "Error on $_\n"
 	} elsif (system("cmp buffer.txt $out") != 0) {
